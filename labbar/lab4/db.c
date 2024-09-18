592497a86ee1aad39251370b6f1552a5fd39e5e1
Symet <salhadi402@gmail.com>
2024-09-18 02:38:08 +0200

    lab5

--- labbar/lab4/db.c
+++ labbar/lab4/db.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "../lab2/utils.h"
 
 struct item
@@ -69,10 +71,45 @@ item_t input_item(){
   return make_item(name, desc, price, lager);
 }
 
+void list_db(item_t *items, int no_items){
+  for (int i = 0; i < no_items; i++)
+    {
+      printf("%d. ", i+1);
+      print_item(&items[i]);
+    }
+}
+
+void edit_db(item_t *items){
+  int num = ask_question_int("What number do you want to edit?");
+  print_item(&items[num-1]);
+  items[num-1] = input_item();
+}
+
+void print_menu(){
+    printf("[L]ägga till en vara\n[T]a bort en vara\n[R]edigera en vara\nÅn[g]ra senaste ändringen\nLista [h]ela varukatalogen\n[A]vsluta\n");
+}
+
+
+char ask_question_menu(){
+  print_menu();
+  char *choice = ask_question_string("Choice: ");
+  char *valid = "LlTtRrGgHhAa";
+  for (int i = 0; i < strlen(valid); i++){
+    if (*choice == valid[i] && strlen(choice) == 1){
+      break;
+    }
+    else if (i == strlen(valid) - 1){
+      printf("Answer is invalid!\n");
+      ask_question_menu();
+    }
+  }
+  return toupper(*choice);
+}
 
 int main(int argc, char *argv[])
 {
-  char *array1[] = { "Laser",        "Polka",    "Extra" };
+  ask_question_menu();
+  /* char *array1[] = { "Laser",        "Polka",    "Extra" };
   char *array2[] = { "förnicklad",   "smakande", "ordinär" };
   char *array3[] = { "skruvdragare", "kola",     "uppgift" };
 
@@ -84,9 +121,10 @@ int main(int argc, char *argv[])
   {
     item_t db[16]; // Array med plats för 16 varor
     int db_siz = 0; // Antalet varor i arrayen just nu
-
+    
     int items = atoi(argv[1]); // Antalet varor som skall skapas
 
+    srand(time(NULL));
     if (items > 0 && items <= 16)
     {
       for (int i = 0; i < items; ++i)
@@ -102,28 +140,28 @@ int main(int argc, char *argv[])
       puts("Sorry, must have [1-16] items in database.");
       return 1; // Avslutar programmet!
     }
-
+      
     for (int i = db_siz; i < 16; ++i)
       {
         char *name = magick(array1, array2, array3, 3); // TODO: Lägg till storlek
         char *desc = magick(array1, array2, array3, 3); // TODO: Lägg till storlek
+        
         int price = rand() % 200000;
         char shelf[] = { rand() % ('Z'-'A') + 'A',
                          rand() % 10 + '0',
                          rand() % 10 + '0',
                          '\0' };
-        item_t item = make_item(name, desc, price, shelf);
+        item_t item = make_item(name, desc, price, strdup(shelf));
 
         db[db_siz] = item;
         ++db_siz;
       }
 
      // Skriv ut innehållet
-     for (int i = 0; i < db_siz; ++i)
-     {
-       print_item(&db[i]);
-     }
+    list_db(db, db_siz);
+    edit_db(db);
+    list_db(db, db_siz);
 
-  }
+  } */
   return 0;
 }

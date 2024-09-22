5943917b1c0d3347790d895342e84547bbfe0b72
unknown <salhadi402@gmail.com>
2024-09-23 01:51:44 +0200

    ioopm_hash_table_keys

--- inluppar/inlupp1/hashtable.c
+++ inluppar/inlupp1/hashtable.c
@@ -161,3 +161,22 @@ void ioopm_hash_table_clear(ioopm_hash_table_t *ht) {
         ht->buckets[i] = NULL; // Set bucket to NULL after clearing the memory 
     }
 }
+
+/// @brief return the keys for all entries in a hash map (in no particular order, but same as ioopm_hash_table_values)
+/// @param h hash table operated upon
+/// @return an array of keys for hash table h
+int *ioopm_hash_table_keys(ioopm_hash_table_t *ht) {
+    int size = ioopm_hash_table_size(ht);
+    
+    int *keys = malloc(size * sizeof(int));
+    int counter = 0;
+    for (int i = 0; i < No_Buckets; i++) {
+        entry_t *entry = ht->buckets[i];
+        if (entry != NULL){
+            keys[counter] = entry->key;
+            entry = entry->next;
+            counter++;
+        }
+    }
+    return keys;
+}
